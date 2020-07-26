9c1e8e86c979e625b58952ea5182ade817758f8d
Aanand Kainth <aanand@akainth.me>
2020-07-26 12:07:58 -0700

    Fetch natural language annotations from sidebar

--- Concept.js
+++ Concept.js
@@ -11,15 +11,51 @@
 function onOpen() {
     DocumentApp.getUi()
         .createAddonMenu()
-        .addItem("Analyze", 'runAnalysis')
+        .addItem("Analyze", 'showSidebar')
         .addToUi();
 }
 
 /**
  * Show the sidebar to the user (only works with the regular add-on, not the mobile version)
  */
-function runAnalysis() {
+function showSidebar() {
     const sidebar = HtmlService.createHtmlOutputFromFile("Sidebar");
     sidebar.setTitle("Concept");
     DocumentApp.getUi().showSidebar(sidebar);
+}
+
+/**
+ * Used to include CSS and JS separately from HTML
+ */
+function include(filename) {
+    return HtmlService.createHtmlOutputFromFile(filename)
+        .getContent();
+}
+
+function runAnalysis() {
+    const payload = {
+        document: {
+            type: "PLAIN_TEXT",
+            language: DocumentApp.getActiveDocument().getLanguage(),
+            content: DocumentApp.getActiveDocument().getBody().getText()
+        },
+        features: {
+            extractSyntax: true,
+            extractEntities: true,
+            extractDocumentSentiment: true,
+            extractEntitySentiment: true,
+            classifyText: true,
+        },
+    };
+
+    const annotationsResponse = UrlFetchApp.fetch("https://language.googleapis.com/v1/documents:annotateText", {
+        method: "post",
+        contentType: "application/json",
+        payload: JSON.stringify(payload),
+        headers: {
+            "Authorization": `Bearer ${ScriptApp.getOAuthToken()}`
+        }
+    });
+    let annotationsText = annotationsResponse.getContentText();
+    return JSON.parse(annotationsText);
 }
